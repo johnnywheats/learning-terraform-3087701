63e40b59b5863304e75657c483c099d83908d27f
johnnywheats <134563787+johnnywheats@users.noreply.github.com>
2023-05-25 21:05:19 -0400

    Update 12

--- main.tf
+++ main.tf
@@ -16,8 +16,7 @@ data "aws_ami" "app_ami" {
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version =  "3.14.2"
-
+  
   name = "dev"
   cidr = "10.0.0.0/16"
 
@@ -25,6 +24,8 @@ module "vpc" {
   public_subnets    = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
  
+  enable_nat_gateway = "true"
+
   tags = {
     Terraform = "true"
     Environment = "dev"
