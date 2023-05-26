cdd600ade67ec75298170f3498a36f2f8bc98167
johnnywheats <134563787+johnnywheats@users.noreply.github.com>
2023-05-25 22:54:05 -0400

    Create dev VPC

--- main.tf
+++ main.tf
@@ -47,8 +47,9 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  name    = "blog"
+    name    = "blog"
+    vpc_id  = module.vpc.public_subnets[0]
+  
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
